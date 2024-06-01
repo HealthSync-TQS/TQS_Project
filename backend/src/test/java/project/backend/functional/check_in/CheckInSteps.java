d6cc9c51ba0707141b59bfa409d935d91d6912e9
beernardoc <bernardopinto@ua.pt>
2024-06-02 00:05:21 +0100

    update checkin functional test

--- backend/src/test/java/project/backend/functional/check_in/CheckInSteps.java
+++ backend/src/test/java/project/backend/functional/check_in/CheckInSteps.java
@@ -1,5 +1,6 @@
 package project.backend.functional.check_in;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en_scouse.An;
@@ -10,6 +11,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -34,6 +36,8 @@ public class CheckInSteps {
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+
+
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(string);
@@ -41,23 +45,23 @@ public class CheckInSteps {
 
     }
 
-    @When("I search for appointment with id {string}")
+    @And("I search for appointment with id {string}")
     public void i_search_for_appointment_with_id(String id) {
         WebElement inputElement = driver.findElement(By.cssSelector("#inputId"));
         inputElement.sendKeys(id);
     }
 
-    @When("I search for appointment with patient {string}")
+    @And("I search for appointment with patient {string}")
     public void i_search_for_appointment_with_patient(String utente) {
         WebElement inputElement = driver.findElement(By.cssSelector("#inputUtente"));
         inputElement.sendKeys(utente);
     }
 
-    @When("I click on the search button")
+    @And("I click on the search button")
     public void i_click_on_the_search_button() {
         WebElement searchButton = driver.findElement(By.xpath("//button[text()='Search']"));
-        searchButton.click();
-
+        Actions actions = new Actions(driver);
+        actions.moveToElement(searchButton).click().perform();
 
     }
 
@@ -74,41 +78,32 @@ public class CheckInSteps {
     }
 
 
-    //            And I click on the payment button
-    @When("I click on the payment button")
+    @And("I click on the payment button")
     public void i_click_on_the_payment_button() {
-        WebElement paymentButton = driver.findElement(By.id("payButton"));
-        paymentButton.click();
+
+        WebElement payButton = driver.findElement(By.id("payButton"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(payButton).click().perform();
     }
 
     //            And I click on the confirm payment button
-    @When("I click on the confirm payment button")
+    @And("I click on the confirm payment button")
     public void i_click_on_the_confirm_payment_button() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement confirmPaymentButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("confirmPaymentButton")));
-        confirmPaymentButton.click();
+        Actions actions = new Actions(driver);
+        actions.moveToElement(confirmPaymentButton).click().perform();
+        driver.quit();
 
-    }
-
-    @Then("I should see {string} button")
-    public void i_should_see_button(String text) {
-        WebElement checkinButton = driver.findElement(By.id("paid"));
-        assertTrue(checkinButton.getText().contains(text));
     }
 
 //             And I click on the checkin button
-    @When("I click on the checkin button")
+    @Then("I click on the checkin button")
     public void i_click_on_the_checkin_button() {
         WebElement checkinButton = driver.findElement(By.id("checkin"));
-        checkinButton.click();
-    }
-
-
-    //             Then I clinck on the "Get Ticket" button
-    @Then("I click on the get ticket button")
-    public void i_click_on_the_button() {
-        WebElement getTicketButton = driver.findElement(By.id("getTicket"));
-        getTicketButton.click();
+        Actions actions = new Actions(driver);
+        actions.moveToElement(checkinButton).click().perform();
+        driver.quit();
     }
 
 
