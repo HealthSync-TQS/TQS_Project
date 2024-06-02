21a84df168b0a161c0a37d3f822f766856f0a6f9
beernardoc <bernardopinto@ua.pt>
2024-06-02 21:01:32 +0100

    fix conflict

--- backend/src/main/java/project/backend/DataInitializer.java
+++ backend/src/main/java/project/backend/DataInitializer.java
@@ -5,12 +5,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import project.backend.entity.Appointment;
 import project.backend.entity.Patient;
+import project.backend.repository.AppointmentRepo;
+import project.backend.repository.PatientRepo;
 import project.backend.service.AppointmentService;
 import project.backend.service.PatientService;
 
 import java.time.LocalTime;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.logging.Logger;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
@@ -18,6 +20,8 @@ public class DataInitializer implements CommandLineRunner {
     private final PatientService patientService;
 
     private final AppointmentService appointmentService;
+
+    private static final Logger logger = Logger.getLogger(DataInitializer.class.getName());
 
     @Autowired
     public DataInitializer(PatientService patientService, AppointmentService appointmentService) {
@@ -33,8 +37,6 @@ public class DataInitializer implements CommandLineRunner {
 
         String speciality1 = "Cardiology";
         String speciality2 = "Dermatology";
-        String doctor1 = "Dr. Smith";
-        String doctor2 = "Dr. Johnson";
         String speciality3 = "General Practice";
         String speciality4 = "Gynecology";
 
@@ -107,12 +109,6 @@ public class DataInitializer implements CommandLineRunner {
         Appointment appointment47 = new Appointment(new Date(124, Calendar.OCTOBER, 25), speciality3, doctor3, "USF Gama ", LocalTime.now(), 100.0);
         Appointment appointment48 = new Appointment(new Date(124, Calendar.OCTOBER, 30), speciality4, doctor4, "Centro de Saude Delta", LocalTime.now(),150.0);
 
-
-        Appointment appointment1 = new Appointment(patient1, new Date(), speciality1, doctor1, "Centro de Saude Delta",  LocalTime.now(), 100.0, false);
-        Appointment appointment2 = new Appointment(patient2, new Date(), speciality2, doctor2, "USF Gama",  LocalTime.now(), 150.0, true);
-        Appointment appointment3 = new Appointment(new Date(124, Calendar.NOVEMBER, 12), speciality1, doctor1, "USF Gama", LocalTime.now(), 100.0);
-        Appointment appointment4 = new Appointment(new Date(124, Calendar.NOVEMBER, 12), speciality2, doctor2, "Centro de Saude Delta", LocalTime.now(),150.0);
-        Appointment appointment5 = new Appointment(new Date(124, Calendar.JUNE, 2), speciality1, doctor1, "USF Gama ", LocalTime.now(), 100.0);
 
         appointmentService.addAppointment(appointment1);
         appointmentService.addAppointment(appointment2);
